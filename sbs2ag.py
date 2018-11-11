63232ad4f2bcd5c963695cd45949c8f10b9e6b4b
qnzy <yves.kunz@gmail.com>
2018-11-11 15:05:32 +0100

    Add files via upload

--- sbs2ag.py
+++ sbs2ag.py
@@ -1,32 +1,39 @@
-import numpy as np
+from __future__ import print_function
 import sys
+import numpy as np
 import cv2
 
 if len(sys.argv) != 2:
     print('give input video as command line argument')
 
-videoName = sys.argv[1]
-videoIn = cv2.VideoCapture(videoName)
-fourcc   = cv2.VideoWriter_fourcc(*'MJPG')
+videoName  = sys.argv[1]
+videoIn    = cv2.VideoCapture(videoName)
+fps        = int(videoIn.get(cv2.CAP_PROP_FPS))
+width      = int(videoIn.get(cv2.CAP_PROP_FRAME_WIDTH))
+height     = int(videoIn.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frameCount = int(videoIn.get(cv2.CAP_PROP_FRAME_COUNT))
+fourcc     = cv2.VideoWriter_fourcc(*'MJPG')
+#fourcc     = int(videoIn.get(cv2.CAP_PROP_FOURCC))
+videoOut   = cv2.VideoWriter('output.avi', fourcc, fps, (width//2, height))
 
-fps    = int(videoIn.get(cv2.CAP_PROP_FPS))
-width  = int(videoIn.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(videoIn.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-#print(f'{width}x{height} {fps}fps')
-
-videoOut = cv2.VideoWriter('output.avi', fourcc, fps, (width//2, height))
-
+anagl = np.zeros((height, width//2, 3)).astype(np.uint8)
+currentFrame = 0
+frameProgress = 0
+print('converting: ', end='')
+sys.stdout.flush()
 while (videoIn.isOpened()):
     ret, frame = videoIn.read()
     if frame is  None:
         break
     h,w,c = frame.shape
-    anagl = np.zeros((h, w//2, 3)).astype(np.uint8)
-    if c != 3:
-        print('error: should be 3 channel frames')
-    if w != width:
-        print('error: wrong frame width')
+    if w != width or h != height or c != 3:
+        print('frame dimension error')
+        break
+    currentFrame += 1
+    if currentFrame - frameProgress >= frameCount/10:
+        print('.', end='')
+        sys.stdout.flush()
+        frameProgress = currentFrame
     left = frame[:,:w//2,:]
     right = frame[:,w//2:,:]
     # 0: b, 1: g, 2: r
@@ -34,8 +41,8 @@ while (videoIn.isOpened()):
     anagl[:,:,1] = right[:,:,1]
     anagl[:,:,2] = left[:,:,2]
     videoOut.write(anagl)
+print('done')
 
 videoIn.release()
 videoOut.release()
-cv2.destroyAllWindows()
 
